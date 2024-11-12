cf2e59d6c6a13d777f88748024d0daf802ba3357
DussanFreire <dussanfreire@gmail.com>
2024-11-12 19:45:09 -0400

    deploy: .env mod

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,7 +2,6 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
-import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
 import { MealInterface } from "./interfaces";
 
